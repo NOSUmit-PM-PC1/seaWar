55004adae334a4ea1ab57a12531d8e2df2ac0525
Makarenkomd <mahadrum@yandex.ru>
2021-01-02 21:39:50 +0300

    Добавьте файлы проекта.

---
+++ seaWar/FormGame.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace seaWar
+{
+    partial class FormGame
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridMy = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridMy)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridMy
+            // 
+            this.dataGridMy.AllowUserToAddRows = false;
+            this.dataGridMy.AllowUserToDeleteRows = false;
+            this.dataGridMy.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridMy.Location = new System.Drawing.Point(29, 25);
+            this.dataGridMy.Name = "dataGridMy";
+            this.dataGridMy.ReadOnly = true;
+            this.dataGridMy.Size = new System.Drawing.Size(482, 437);
+            this.dataGridMy.TabIndex = 0;
+            // 
+            // FormGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 500);
+            this.Controls.Add(this.dataGridMy);
+            this.Name = "FormGame";
+            this.Text = "Морской бой";
+            this.Load += new System.EventHandler(this.FormGame_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridMy)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridMy;
+    }
+}
+
---
+++ seaWar/FormGame.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace seaWar
+{
+    public partial class FormGame : Form
+    {
+        public FormGame()
+        {
+            InitializeComponent();
+        }
+
+        private void FormGame_Load(object sender, EventArgs e)
+        {
+            int cellSize = 40;
+            dataGridMy.RowHeadersVisible = false;
+            
+            dataGridMy.Columns.Add("index", "");
+            dataGridMy.Columns[0].Width = cellSize;
+            for (int i = 0; i < 10; i++)
+            {
+                //DataGridViewImageColumn tempColumn = new DataGridViewImageColumn();
+                //dataGridMy.Columns.Add(tempColumn);
+  
+                dataGridMy.Columns.Add(new DataGridViewImageColumn());
+                //dataGridMy.Columns.Add("index", Convert.ToChar(Convert.ToInt32('A') + i).ToString());
+                dataGridMy.Columns[i + 1].Width = cellSize;
+               // dataGridMy.Columns[i + 1].ValueType = DataGridViewImageColumn;
+
+            }
+
+            dataGridMy.RowTemplate.Height = cellSize;
+            for (int i = 0; i < 10; i++)
+            {
+                dataGridMy.Rows.Add();
+                dataGridMy.Rows[i].Cells[0].Value = (i + 1).ToString();
+            }
+
+            Image img_ship = new Bitmap("ship_small.png");
+            Image img_sea = new Bitmap("sea_small.png");
+            for (int x = 1; x < 11; x++)
+            {
+                for (int y = 0; y < 10; y++)
+                {
+                    dataGridMy.Rows[y].Cells[x].Value = img_sea;
+                }
+            }
+
+            dataGridMy.Rows[5].Cells[3].Value = img_ship;
+
+
+        }
+    }
+}
